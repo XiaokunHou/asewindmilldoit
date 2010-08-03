a4e6da423034c41019880e3dece5bb1921daad16
chengzhexu2008@gmail.com <chengzhexu2008@gmail.com@56cc2c95-fc8f-3840-5970-3285d62fc4d9>
2010-08-03 19:35:28 +0000

    box初始版

--- trunk/ asewindmilldoit/Windmillase/src/cn/edu/nju/software/box/ContactBox.java
+++ trunk/ asewindmilldoit/Windmillase/src/cn/edu/nju/software/box/ContactBox.java
@@ -1,9 +1,106 @@
 package cn.edu.nju.software.box;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import cn.edu.nju.software.control.LocalDataControl;
+import cn.edu.nju.software.database.Contact;
 import cn.edu.nju.software.database.Task;
 
 public class ContactBox extends CollectBox{
-  //参照其他三个Box
+  
+	ArrayList<Contact> allcontact;
+	LocalDataControl dc;
+	ArrayList<String> Lun = new ArrayList<String>(); //得到该用户的所有的联系人名称
+	ArrayList<Group> groups = new ArrayList<Group>();
+	
+	Group currentGroup;
+	
+	public void setContactList(LocalDataControl x){
+		dc=x;
+		allcontact=x.getContact();
+	}
+	
+	public void setCurrentGroup(Group x){
+		currentGroup=x;
+	}
+	
+	public void spiltIntoGroup(){
+		for(Iterator<Contact> it = allcontact.iterator();it.hasNext();){
+			Contact tk = it.next();
+			String adder_username=tk.getadder_username();
+			while(!Lun.contains(adder_username)){
+				//pn.add(j, projectname);
+				//j++;
+				Lun.add(adder_username);
+			}
+		}
+		for(int i=0;i<Lun.size();i++){
+			Group g = new Group();
+			//m.setProjectName(Lun.get(i));
+			g.setContactName(Lun.get(i));
+			for(Iterator<Contact> it = allcontact.iterator();it.hasNext();){
+				Contact tk = it.next();
+				if(tk.getadder_username().equals(g.getContactName())){
+					g.addContactInGroup(tk);
+				}
+			}
+			groups.add(g);
+		}
+		writeGroupInFile(); //写入文件
+	}
+	
+	public ArrayList<Group> getAllGroup(){
+		return groups;
+	}
+	
+	public void writeGroupInFile(){
+		//当有addTask()等操作后调用
+		//把projects写入文件
+	try{	
+		File file=new File("LocalData/groups.txt");
+		FileOutputStream output=new FileOutputStream(file);
+		ObjectOutputStream  x=new ObjectOutputStream(output);
+		x.writeObject(groups);
+		x.close();
+	 }catch(Exception x){
+		 System.out.println("写入文件错误");
+	 }
+	}
+	public void readGroupFromFile(){
+		//不联网时候调用从本地文件读入ArrayList<Project>  放入projects
+		try{	
+			File file=new File("LocalData/groups.txt");
+			FileInputStream output=new FileInputStream(file);
+			ObjectInputStream  x=new ObjectInputStream(output);
+			groups=(ArrayList<Group>)x.readObject();
+			x.close();
+		 }catch(Exception x){
+			 System.out.println("读入文件错误");
+		 }
+	}
+	
+	//下面是给GUI接口，在一个模式的分类下增删等操作
+	public void addContact(Contact x){
+		currentGroup.addContactInGroup(x);
+		writeGroupInFile();
+	}
+	
+	public void deleteContact(Contact x){
+		currentGroup.deleteContactInGroup(x);
+		writeGroupInFile();
+	}
+	
+	public void editContact(Contact x){
+		currentGroup.editContactInGroup(x);
+		writeGroupInFile();
+	}
+	
 	@Override
 	public void addTask(Task x) {
 		// TODO Auto-generated method stub
--- trunk/ asewindmilldoit/Windmillase/src/cn/edu/nju/software/box/Scene.java
+++ trunk/ asewindmilldoit/Windmillase/src/cn/edu/nju/software/box/Scene.java
@@ -6,30 +6,51 @@ import cn.edu.nju.software.database.Task;
 
 public class Scene {
 	ArrayList<Task> tasks=new ArrayList<Task>();
-	String name;//情景名字
-	public String getName(){
-		   return name;
+	String scenename;//情景名字
+	int index;
+	public String getSceneName(){
+		   return scenename;
 	}
-	public void setName(String x){
-		   name=x;
+	public void setSceneName(String x){
+		   scenename=x;
 	}
 	   public void addTaskInScene(Task x){
 		  
 		   //add的时候可以直接tasks.add()
+		   tasks.add(x);
 	   }
-	   public void deleteTaskInScene(Task k){
+	   public void deleteTaskInScene(Task x){
 		   //Task的delete属性为True，即塞入垃圾箱
 		   //调用findTask()然后从tasks中删除
+		   findTask(x);
+		   tasks.get(index).setisdelete(true);
 	   }
 	   public void editTaskInScene(Task x){
 		   //编辑Task，修改
 		 //调用findTask()然后从tasks中修改
+		   findTask(x);
+		   tasks.get(index).setprojectname(x.getprojectname());
+		   tasks.get(index).setscenename(x.getscenename());
+		   tasks.get(index).settaskendtime(x.gettaskendtime());
+		   tasks.get(index).settaskinformation(x.gettaskinformation());
+		   tasks.get(index).settasklabel(x.gettasklabel());
+		   tasks.get(index).settaskname(x.gettaskname());
+		   tasks.get(index).settaskpriority(x.gettaskpriority());
+		   tasks.get(index).settaskshared(x.gettaskshared());
+		   tasks.get(index).settaskstarttime(x.gettaskstarttime());
 	   }
 	   public void completeTaskInScene(Task x){
 		 //调用findTask()然后从tasks中找到然后设置他的hasdone为true
+		   findTask(x);
+		   tasks.get(index).setisdone(true);
 	   }
-	   public Task findTask(Task x){
+	   public int findTask(Task x){
 		   //根据Task的name找到tasks中的所在Task并return
-		   return null;
+		   index=0;
+			while(!(tasks.get(index).gettaskname()).equals(x.gettaskname())){
+				index++;
+			}
+	   
+	   return index;
 	   }  
 }
